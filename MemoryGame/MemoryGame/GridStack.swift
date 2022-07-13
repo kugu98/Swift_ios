19f5b896271285d6b652f230682c845ccf699c08
Hyun Gu lee <82009882+kugu98@users.noreply.github.com>
2022-07-13 22:25:25 +0900

    GridStack
    GridStack catch error

--- MemoryGame/MemoryGame/GridStack.swift
+++ MemoryGame/MemoryGame/GridStack.swift
@@ -4,29 +4,30 @@
 //
 //  Created by 이현구 on 2022/07/13.
 //
-
 import SwiftUI
-struct GridStack<Content: View>: View{
+
+struct GridStack<Content: View>: View {
     let rows: Int
     let columns: Int
-    let content:(_ row :Int, _ column: Int) -> Content
-    var body: some View{
-        VStack{
-            ForEach(0..<rows,id: \.self){
-                row in
-                HStack{
-                    ForEach(0..<columns,id: \.self){
-                        col in
-                        Text("\(col),\(row)")
-                            .frame(maxWidth: .infinity, minHeight:.infinity)
+    let content: (_ row: Int, _ column: Int) -> Content
+    var body: some View {
+        VStack {
+            ForEach (0 ..< rows, id: \.self) { row in
+                HStack {
+                    ForEach (0 ..< columns, id: \.self) { col in
+                        self.content(row, col)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
                 }
             }
         }
     }
 }
-struct GridStack_Previews: PreviewProvider{
-    static var previews: some View{
-        GridStack(rows: 4, columns: 3){r, c in Text ("\(r),\(c)")}
+
+struct GridStack_Previews: PreviewProvider {
+    static var previews: some View {
+        GridStack(rows: 4, columns: 3) { r, c in
+            Text("\(r),\(c)")
+        }
     }
 }
