37f227e3e80d624c9fe7f5a9aaf80e95c9aec426
Hyun Gu lee <82009882+kugu98@users.noreply.github.com>
2022-07-14 11:36:41 +0900

    GameMemory

--- MemoryGame/MemoryGame/CardView.swift
+++ MemoryGame/MemoryGame/CardView.swift
@@ -7,36 +7,56 @@
 
 import SwiftUI
 
-struct CardView: View{
-    enum CardState{
-        case removed, closed, open
-    }
-    var state: CardState
-    var prefix:String
+struct CardView: View {
+    var state: Card.State
+    var prefix: String
     var num: Int
-    var body: some View{
-        if state == .removed{
+    var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State var frameIndex = 1
+    var body: some View {
+        if state == .removed {
             Image(systemName: "x.circle")
                 .resizable()
+//                .aspectRatio(contentMode: .fit)
                 .opacity(0.1)
-        }else{
+        } else {
             Image(imageName)
+                .onReceive(timer) { _ in
+//                    NSLog("on timer receivw")
+                    frameIndex += 1
+                    if frameIndex > 8 {
+                        frameIndex = 1
+                    }
+                }
         }
     }
-    var imageName: String{
-        if state == .closed{
-            return prefix+"_back"
+    var imageName: String {
+        if state == .closed {
+            return prefix + "_back"
         }
-        return prefix+String(format: "_%02d_01", num)
+        return prefix + String(format: "_%02d_%02d", num, frameIndex)
     }
 }
-struct CardView_previews : PreviewProvider{
-    static var previews: some View{
-        VStack{
-            CardView(state:.open, prefix:"f",num: 1)
-            CardView(state:.open, prefix:"f",num: 2)
-            CardView(state:.closed, prefix:"t",num: 1)
-            CardView(state:.removed, prefix:"t",num: 1)
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HStack {
+                CardView(state:.open, prefix: "f", num: 1)
+                CardView(state:.closed, prefix: "f", num: 1)
+            }
+            HStack {
+                CardView(state:.open, prefix: "f", num: 2)
+                CardView(state:.closed, prefix: "f", num: 2)
+            }
+            HStack {
+                CardView(state:.open, prefix: "t", num: 3)
+                CardView(state:.closed, prefix: "t", num: 3)
+            }
+            HStack {
+                CardView(state:.removed, prefix: "f", num: 3)
+                CardView(state:.removed, prefix: "t", num: 3)
+            }
         }
     }
 }
